4a2bebbbf325d2256eee97c0f8a1f01c88dc7469
Filip Czaplicki <github@starsep.com>
2022-04-15 23:28:00 +0200

    disused-route

---
+++ disused-route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from itertools import groupby
+import overpy
+
+
+def downloadDisusedRoutes():
+    query = f"""
+        area["name"="Warszawa"]["admin_level"=6];
+        (
+            relation["disused:route"]["name"](area);
+        );
+        out center;
+    """
+    overpassResult = overpy.Overpass().query(query)
+    return overpassResult.relations
+
+
+disusedRoutes = downloadDisusedRoutes()
+
+
+def disusedRouteType(route):
+    return route.tags["disused:route"]
+
+
+for vehicle, routes in groupby(
+    sorted(disusedRoutes, key=disusedRouteType), key=disusedRouteType
+):
+    print()
+    print(vehicle)
+    for route in sorted(list(routes), key=lambda route: route.tags["name"]):
+        print(route.tags["name"], f"https://osm.org/relation/{route.id}")

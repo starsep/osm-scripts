3c6224517c9c8163d44db58189d2f65bb32b97a5
Filip Czaplicki <github@starsep.com>
2024-09-05 18:25:59 +0200

    Normalize

--- taginfoCountPerCountry.py
+++ taginfoCountPerCountry.py
@@ -1,64 +1,154 @@
 #!/usr/bin/env python
 import argparse
 import asyncio
+from dataclasses import dataclass
 
 import httpx
 from tqdm.asyncio import tqdm
 
 
-LANG_PL = "pl"
-LANG_EN = "en"
+class LANG:
+    PL = "pl"
+    EN = "en"
+
+
+class ISO:
+    WORLD = "WORLD"
+    EUROPE = "EUROPE"
+
+
+class NORMALIZE:
+    POPULATION = "population"
+    AREA = "area"
+
+
+@dataclass(frozen=True)
+class Country:
+    nameEn: str
+    namePl: str
+    taginfo: str
+    isoCode: str
+
+    @property
+    def symbol(self):
+        match self.isoCode:
+            case ISO.WORLD:
+                return ":world_map:"
+            case ISO.EUROPE:
+                return ":flag-eu:"
+            case _:
+                return f":flag-{self.isoCode.lower()}:"
+
+    @property
+    def host(self):
+        if self.isoCode == ISO.WORLD:
+            return "https://taginfo.openstreetmap.org/"
+        return f"https://taginfo.geofabrik.de/{self.taginfo}/"
+
 
 COUNTRIES = {
-    ("World", "Świat", "https://taginfo.openstreetmap.org/", ":world_map:"),
-    ("Europe", "Europa", "https://taginfo.geofabrik.de/europe/", ":flag-eu:"),
-    ("Poland", "Polska", "https://taginfo.geofabrik.de/europe:poland/", ":flag-pl:"),
-    ("Germany", "Niemcy", "https://taginfo.geofabrik.de/europe:germany/", ":flag-de:"),
-    ("France", "Francja", "https://taginfo.geofabrik.de/europe:france/", ":flag-fr:"),
-    ("Spain", "Hiszpania", "https://taginfo.geofabrik.de/europe:spain/", ":flag-es:"),
-    ("UK", "UK", "https://taginfo.geofabrik.de/europe:united-kingdom/", ":flag-gb:"),
-    ("Italy", "Włochy", "https://taginfo.geofabrik.de/europe:italy/", ":flag-it:"),
-    (
-        "Czechia",
-        "Czechy",
-        "https://taginfo.geofabrik.de/europe:czech-republic/",
-        ":flag-cz:",
-    ),
-    ("Russia", "Rosja", "https://taginfo.geofabrik.de/russia/", ":flag-ru:"),
-    ("Turkey", "Turcja", "https://taginfo.geofabrik.de/europe:turkey/", ":flag-tr:"),
-    ("Ukraine", "Ukraina", "https://taginfo.geofabrik.de/europe:ukraine/", ":flag-ua:"),
-    ("Romania", "Rumunia", "https://taginfo.geofabrik.de/europe:romania/", ":flag-ro:"),
-    (
-        "Netherlands",
-        "Holandia",
-        "https://taginfo.geofabrik.de/europe:netherlands/",
-        ":flag-nl:",
-    ),
-    (
-        "Slovakia",
-        "Słowacja",
-        "https://taginfo.geofabrik.de/europe:slovakia/",
-        ":flag-sk:",
-    ),
-    ("Belgium", "Belgia", "https://taginfo.geofabrik.de/europe:belgium/", ":flag-be:"),
-    ("Hungary", "Węgry", "https://taginfo.geofabrik.de/europe:hungary/", ":flag-hu:"),
-    ("Austria", "Austria", "https://taginfo.geofabrik.de/europe:austria/", ":flag-at:"),
-    ("Belarus", "Białoruś", "https://taginfo.geofabrik.de/europe:belarus/", ":flag-by:"),
-    ("Lithuania", "Litwa", "https://taginfo.geofabrik.de/europe:lithuania/", ":flag-lt:"),
-    ("Canada", "Kanada", "https://taginfo.geofabrik.de/north-america:canada/", ":flag-ca:"),
-    ("USA", "USA", "https://taginfo.geofabrik.de/north-america:us/", ":flag-us:"),
-    ("Brazil", "Brazylia", "https://taginfo.geofabrik.de/south-america:brazil/", ":flag-br:"),
-    ("China", "Chiny", "https://taginfo.geofabrik.de/asia:china/", ":flag-cn:"),
-    ("Japan", "Japonia", "https://taginfo.geofabrik.de/asia:japan/", ":flag-jp:"),
+    Country("World", "Świat", "", ISO.WORLD),
+    Country("Europe", "Europa", "europe", ISO.EUROPE),
+    Country("Poland", "Polska", "europe:poland", "PL"),
+    Country("Germany", "Niemcy", "europe:germany", "DE"),
+    Country("France", "Francja", "europe:france", "FR"),
+    Country("Spain", "Hiszpania", "europe:spain", "ES"),
+    Country("UK", "UK", "europe:united-kingdom", "GB"),
+    Country("Italy", "Włochy", "europe:italy", "IT"),
+    Country("Czechia", "Czechy", "europe:czech-republic", "CZ"),
+    Country("Russia", "Rosja", "russia", "RU"),
+    Country("Turkey", "Turcja", "europe:turkey", "TR"),
+    Country("Ukraine", "Ukraina", "europe:ukraine", "UA"),
+    Country("Romania", "Rumunia", "europe:romania", "RO"),
+    Country("Netherlands", "Holandia", "europe:netherlands", "NL"),
+    Country("Slovakia", "Słowacja", "europe:slovakia", "SK"),
+    Country("Belgium", "Belgia", "europe:belgium", "BE"),
+    Country("Hungary", "Węgry", "europe:hungary", "HU"),
+    Country("Austria", "Austria", "europe:austria", "AT"),
+    Country("Belarus", "Białoruś", "europe:belarus", "BY"),
+    Country("Lithuania", "Litwa", "europe:lithuania", "LT"),
+    Country("Canada", "Kanada", "north-america:canada", "CA"),
+    Country("USA", "USA", "north-america:us", "US"),
+    Country("Brazil", "Brazylia", "south-america:brazil", "BR"),
+    Country("China", "Chiny", "asia:china", "CN"),
+    Country("Japan", "Japonia", "asia:japan", "JP"),
 }
 
 client = httpx.AsyncClient()
 
 
-async def taginfoCount(key: str, value: str, host: str, label: str) -> tuple[str, int]:
+@dataclass
+class TaginfoResult:
+    label: str
+    count: int
+    isoCode: str
+
+
+async def taginfoCount(
+    key: str, value: str, host: str, label: str, isoCode: str
+) -> TaginfoResult:
     response = await client.get(f"{host}api/4/tag/stats?key={key}&value={value}")
     response.raise_for_status()
-    return label, response.json()["data"][0]["count"]
+    return TaginfoResult(label, response.json()["data"][0]["count"], isoCode)
+
+
+@dataclass
+class WikidataResult:
+    population: int
+    area: int
+
+
+async def queryWikidata():
+    countryIsoCodes = "({})".format(
+        ", ".join(
+            [
+                f'"{country.isoCode}"'
+                for country in COUNTRIES
+                if country.isoCode not in (ISO.WORLD, ISO.EUROPE)
+            ]
+        )
+    )
+    queryCountries = f"""
+    SELECT ?item ?isoCode ?population ?area WHERE {{
+      ?item wdt:P1082 ?population.
+      ?item wdt:P2046 ?area.
+      ?item wdt:P297 ?isoCode.
+      FILTER(?isoCode IN {countryIsoCodes})
+    }}"""
+    querySpecials = f"""
+    SELECT ?item ?isoCode ?population ?area WHERE {{
+      ?item wdt:P1082 ?population.
+      ?item wdt:P2046 ?area.
+      FILTER(?item IN (wd:Q2, wd:Q46))
+      BIND(IF(?item=wd:Q2, "{ISO.WORLD}", "{ISO.EUROPE}") AS ?isoCode).
+    }}"""
+    query = f"""
+        SELECT * {{
+        {{
+            {queryCountries}
+        }} 
+        UNION 
+        {{
+            {querySpecials}
+        }} 
+        }} ORDER BY DESC(?population)
+    """
+    response = await client.get(
+        "https://query.wikidata.org/sparql", params={"query": query, "format": "json"}
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+async def getWikidataResults() -> dict[str, WikidataResult]:
+    data = await queryWikidata()
+    results = {}
+    for item in data["results"]["bindings"]:
+        isoCode = item["isoCode"]["value"]
+        population = int(float(item["population"]["value"]))
+        area = int(float(item["area"]["value"]))
+        results[isoCode] = WikidataResult(population, area)
+    return results
 
 
 async def main():
@@ -66,26 +156,43 @@ async def main():
     parser.add_argument("key")
     parser.add_argument("value")
     parser.add_argument(
-        "--lang", default=LANG_EN, choices=[LANG_EN, LANG_PL], required=False
+        "--lang", default=LANG.EN, choices=[LANG.EN, LANG.PL], required=False
+    )
+    parser.add_argument(
+        "--normalize", choices=[NORMALIZE.AREA, NORMALIZE.POPULATION], required=False
     )
     parser.add_argument("--symbol", default=False, required=False, action="store_true")
     args = parser.parse_args()
-    langIndex = 0 if args.lang == LANG_EN else 1
-    results = await tqdm.gather(
+    results: list[TaginfoResult] = await tqdm.gather(
         *[
             taginfoCount(
                 args.key,
                 args.value,
-                host,
-                f"{symbol + ' ' if args.symbol else ''}{(nameEn, namePl)[langIndex]}",
+                country.host,
+                f"{country.symbol + ' ' if args.symbol else ''}"
+                + f"{country.nameEn if args.lang == LANG.EN else country.namePl}",
+                country.isoCode,
             )
-            for (nameEn, namePl, host, symbol) in COUNTRIES
+            for country in COUNTRIES
         ],
         desc="↔️ Checking taginfo",
     )
-    results = sorted(results, key=lambda x: x[1], reverse=True)
-    for result in results:
-        print(f"{result[0]}: {result[1]}")
+    resultsPerIso = {result.isoCode: result for result in results}
+    if args.normalize:
+        wikidataResults = await getWikidataResults()
+        for isoCode in wikidataResults:
+            value = (
+                wikidataResults[isoCode].population
+                if args.normalize == NORMALIZE.POPULATION
+                else wikidataResults[isoCode].area
+            )
+            print(f"iso {isoCode}, ratio {value}, count {resultsPerIso[isoCode].count}")
+            resultsPerIso[isoCode].count = int(
+                1_000_000 * resultsPerIso[isoCode].count / value
+            )
+    output = sorted(list(resultsPerIso.values()), key=lambda x: x.count, reverse=True)
+    for result in output:
+        print(f"{result.label}: {result.count}")
 
 
 if __name__ == "__main__":
